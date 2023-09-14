74ca2f707d247e69c8758365c35dd86f34e64fdc
Mustapha Sherifdeen <musbolaji01@gmail.com>
2023-09-14 21:21:32 +0100

    First commit on flasker

---
+++ hello.py
@@ -0,0 +1,59 @@
+from flask import Flask, render_template, flash
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+app.config['SECRET_KEY'] = 'not to be disclosed'
+
+db = SQLAlchemy(app)
+
+class Users(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(200), nullable=False)
+    email = db.Column(db.String(120), nullable=False, unique=True)
+    date_added = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return '<Name %r>' % self.name
+
+
+class NamerForm(FlaskForm):
+    name = StringField('Whats Your Name', validators=[DataRequired()])
+    submit = SubmitField('submit')
+
+@app.route('/')
+def index():
+    name = 'Kaphy'
+    list = ["car", "lorry", "train"]
+    return render_template('index.html', list=list, name=name)
+
+
+@app.route('/user/<name>')
+def user(name):
+    return render_template('user.html', name=name)
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def page_not_found(e):
+    return render_template('500.html'), 500
+
+@app.route('/name', methods=['GET', 'POST'])
+def name():
+    name = None
+    form = NamerForm()
+    #Validate Form
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+        flash("Form Submitted Successfully! ")
+        
+    return render_template('name.html', name=name, form=form)
